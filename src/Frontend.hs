3817853c14d9c1d8af656ba028a187ea418d59c8
Jake Runzer <jakerunzer@gmail.com>
2018-05-30 23:44:15 -0700

    basic frontend syntax

---
+++ src/Frontend.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Frontend where
+
+import Prelude hiding (foldr, foldr1, concatMap)
+
+import           Name
+import           Type
+
+import           GHC.Word (Word8)
+
+type Constr = Name
+
+data Expr
+  = EApp Expr Expr               -- a b
+  | EVar Name                    -- x
+  | ELam Name Expr               -- \x -> y
+  | ELit Literal                 -- 2, "hello"
+  deriving (Eq, Show)
+
+data Literal
+  = LitInt Int                   -- 1
+  | LitChar Char                 -- 'a'
+  | LitString [Word8]
+  deriving (Eq, Ord, Show)
+
+data BindGroup = BindGroup
+  { _matchName  :: Name
+  , _matchPats  :: [Match]
+  , _matchType  :: Maybe Type
+  } deriving (Eq, Show)
+
+data Match = Match
+  { _matchPat  :: [Pattern]
+  , _matchBody :: Expr
+  } deriving (Eq, Show)
+
+data Pattern
+  = PVar Name                    -- x
+  | PCon Constr [Pattern]        -- C x y
+  | PLit Literal                 -- 3
+  | PWild                        -- _
+  deriving (Eq, Show)
+
+data Decl
+  = FunDecl BindGroup            -- f x = x + 1
+  | TypeDecl Type                -- f :: Int -> Int
+  deriving (Eq, Show)
+
+data Module = Module Name [Decl] -- module T export { .. }
+  deriving (Eq, Show)
---
+++ src/Name.hs
@@ -0,0 +1,30 @@
+module Name where
+
+import           Control.Monad
+import           Data.Monoid
+import           Data.String
+
+data Name
+  = Gen String Integer
+  | Name String
+  deriving (Eq, Ord, Show, Read)
+
+instance IsString Name where
+  fromString = Name
+
+prefix :: String -> Name -> Name
+prefix p (Gen nm i) = Gen (p <> nm) i
+prefix p (Name nm)  = Name (p <> nm)
+
+unName :: IsString a => Name -> a
+unName (Name s)  = fromString s
+unName (Gen s n) = fromString (s ++ show n)
+
+letters :: [String]
+letters = [1..] >>= flip replicateM ['a'..'z']
+
+genNames :: [Name]
+genNames = Prelude.zipWith Gen letters [0..]
+
+class Named a where
+  getName :: a -> Name
