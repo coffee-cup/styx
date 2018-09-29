99171f469badb76529fb68f2be617bcf8c77da45
Jake Runzer <jakerunzer@gmail.com>
2018-09-29 15:59:29 -0600

    remove kinds from inside of types

--- src/Type.hs
+++ src/Type.hs
@@ -4,11 +4,12 @@ module Type where
 
 import           Name
 
+import           Data.List   (foldl')
 import           Data.String
 
 data Type
-  = TVar Tyvar     -- type variable
-  | TCon Tycon     -- constant
+  = TVar TVar     -- type variable
+  | TCon TyCon     -- constant
   | TApp Type Type -- application
   | TArr Type Type -- arrow
   deriving (Eq, Ord, Show)
@@ -16,45 +17,155 @@ data Type
 data Kind
   = KStar
   | KArr Kind Kind
+  | KVar Name
   deriving (Eq, Ord, Show)
 
--- type variable
-data Tyvar = TV Name
+instance Named Type where
+  getName (TVar t) = tvName t
+  getName (TCon const) = case const of
+    AlgTyCon n  -> n
+    PrimTyCon n -> n
+
+-- type variables
+
+data TVar = TV
+  { tvName :: Name
+  } deriving (Eq, Ord, Show)
+
+instance IsString TVar where
+  fromString x = TV (fromString x)
+
+-- type constants
+
+data TyCon
+  = AlgTyCon { tyId :: Name }
+  | PrimTyCon { tyId :: Name }
   deriving (Eq, Ord, Show)
 
--- type constant
-data Tycon = TC Name
-  deriving (Eq, Ord, Show)
+instance IsString TyCon where
+  fromString = AlgTyCon . fromString
 
--- qualified type
-data Qual t = [Pred] :=> t
-  deriving (Eq, Ord, Show)
+-- predicates
 
--- predicate
 data Pred = IsIn Name Type
   deriving (Eq, Ord, Show)
 
-instance IsString Tyvar where
-  fromString = TV . fromString
+-- qualified types
 
-instance IsString Tycon where
-  fromString = TC . fromString
+data Qual t = [Pred] :=> t
+  deriving (Eq, Ord, Show)
 
-instance Named Type where
-  getName (TVar (TV n)) = n
-  getName (TCon (TC n)) = n
+-- alpha equivalence
 
-class HasKind t where
-  kind :: t -> Kind
+class Alpha a where
+  aeq :: a -> a -> Bool
 
-instance HasKind Tyvar where
-  kind (TV _) = KStar
-instance HasKind Tycon where
-  kind (TC _) = KStar
-instance HasKind Type where
-  kind (TVar u) = kind u
-  kind (TCon c) = kind c
-  kind (TApp t _) = case kind t of
-                      (KArr _ k) -> k
-                      k          -> k
-  kind (TArr t _) = kind t
+instance Alpha TVar where
+  aeq _ _ = True
+
+instance Alpha Type where
+  aeq (TVar _) (TVar _)     = True
+  aeq (TApp a b) (TApp c d) = aeq a c && aeq b d
+  aeq (TArr a b) (TArr c d) = aeq a c && aeq b d
+  aeq (TCon a) (TCon b)     = a == b
+  aeq _ _                   = False
+
+instance Alpha Kind where
+  aeq KStar KStar           = True
+  -- aeq KPrim KPrim = True
+  aeq (KArr a b) (KArr c d) = aeq a c && aeq b d
+  aeq _ _                   = False
+
+-- deconstructors
+
+viewTArr :: Type -> [Type]
+viewTArr (TArr t1 t2) = t1 : viewTArr t2
+viewTArr t            = [t]
+
+viewTApp :: Type -> [Type]
+viewTApp t = go t []
+  where
+    go (TApp t1 t2) acc = go t1 (t2:acc)
+    go t1 acc           = (t1 : acc)
+
+typeArity :: Type -> Int
+typeArity ty = length $ viewTArr ty
+
+-- constructors
+
+mkTArr :: [Type] -> Type
+mkTArr []     = error "not defined for empty lists"
+mkTArr [t]    = t
+mkTArr (t:ts) = TArr t (mkTArr ts)
+
+mkTApp :: TyCon -> [Type] -> Type
+mkTApp tcon args = foldl' TApp (TCon tcon) args
+
+mkTPair :: [Type] -> Type
+mkTPair = foldr1 pair
+  where pair x y = mkTApp (AlgTyCon "Pair") [x, y]
+
+mkTList :: Type -> Type
+mkTList tp = TApp (TCon (AlgTyCon "List")) tp
+
+-- build in types
+
+tyInt :: Type
+tyInt = TCon intTyCon
+
+tyDouble :: Type
+tyDouble = TCon doubleTyCon
+
+tyChar :: Type
+tyChar = TCon charTyCon
+
+tyBool :: Type
+tyBool = TCon (AlgTyCon "Bool")
+
+tyList :: Type
+tyList = TCon listTyCon
+
+tyPair :: Type
+tyPair = TCon pairTyCon
+
+tyUnit :: Type
+tyUnit = TCon unitTyCon
+
+tString :: Type
+tString = mkTList tyChar
+
+intTyCon :: TyCon
+intTyCon = PrimTyCon "Int"
+
+doubleTyCon :: TyCon
+doubleTyCon = PrimTyCon "Double"
+
+charTyCon :: TyCon
+charTyCon = PrimTyCon "Char"
+
+listTyCon :: TyCon
+listTyCon = AlgTyCon "List"
+
+pairTyCon :: TyCon
+pairTyCon = AlgTyCon "Pair"
+
+unitTyCon :: TyCon
+unitTyCon = AlgTyCon "Unit"
+
+tyArrow :: Type
+tyArrow = TCon (AlgTyCon "->")
+
+-- class HasKind t where
+--   kind :: t -> Kind
+
+-- instance HasKind Tyvar where
+--   kind (TV _ k) = k
+-- instance HasKind Tycon where
+--   kind (TC _ k) = k
+-- instance HasKind Type where
+--   kind (TVar u) = kind u
+--   kind (TCon c) = kind c
+--   kind (TApp t _) = case kind t of
+--                       (KArr _ k) -> k
+--                       k          -> k
+--   kind (TArr t _) = kind t
